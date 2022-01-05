391c23d3482c1bc2660d8d858ff16182db26626b
manigoldTLC <gabrxavier353@gmail.com>
2022-01-05 14:46:23 -0300

    Added function to create zip file

--- teste2/utils.py
+++ teste2/utils.py
@@ -1,4 +1,5 @@
 import tabula
+from zipfile import *
 
 
 def extract_table(file: str, pages: str, area: tuple) -> list:
@@ -8,3 +9,10 @@ def extract_table(file: str, pages: str, area: tuple) -> list:
 
 def convert_to_csv(l: list, name: str) -> None:
     l[0].to_csv(f"{name}.csv", index=False)
+
+
+def create_zip(file1: str, file2: str, file3: str) -> None:
+    with ZipFile("Teste_Gabriel_Cardoso.zip", "w") as zip:
+        zip.write(f"{file1}.csv")
+        zip.write(f"{file2}.csv")
+        zip.write(f"{file3}.csv")
